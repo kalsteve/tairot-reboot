b6f16870411ce9c1ed2c8859f37594436de03ce7
김윤기 <kalsagansteve@gmail.com>
2025-06-13 00:11:07 +0900

    fix: server setting

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: ['tairot.yukey.site'],
     hmr: false,
     port: 5000,
     host: true,
